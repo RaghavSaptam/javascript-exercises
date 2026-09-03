f2bcaa585ceaaf1c76073c83407fb957a38d1aee
Raghav Singh <108785872+RaghavSaptam@users.noreply.github.com>
2026-09-03 11:01:20 +0530

    Did the 06_repeatString ques

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    if(num == 0){
+        return '';
+    }else if(num < 0){
+        return 'ERROR';
+    }else{
+        let temp = '';
+        for(let i=0; i<num; i++){
+            temp += str;
+        }
+        return temp;
+    }
 };
 
 // Do not edit below this line
