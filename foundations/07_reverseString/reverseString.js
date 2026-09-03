6f5f126703049126c59685ec2065fc5511925e4f
Raghav Singh <108785872+RaghavSaptam@users.noreply.github.com>
2026-09-03 11:16:14 +0530

    write the plan before coding

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let l = str.length;
+    let temp = '';
+    for(let i=l-1; i>=0; i--){
+        temp += str[i];
+    }
+    str = temp;
+    return str;
 };
 
 // Do not edit below this line
