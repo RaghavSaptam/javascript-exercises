33234569bc38bf11a373d8dc99c96b810d2782aa
Raghav Singh <108785872+RaghavSaptam@users.noreply.github.com>
2026-09-03 16:11:45 +0530

    Checking input type

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if(num1 > 0 && num1 % 1 === 0 && typeof num1 === 'number' && num2 > 0 && num2 % 1 === 0 && typeof num2 === 'number'){
+        let i = Math.min(num1, num2);
+        let j = Math.max(num1, num2);
+        let sum = 0;
+        for(; i<=j; i++){
+            sum += i;
+        }
+        return sum;
+    }else{
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
